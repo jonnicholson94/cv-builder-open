b5f43e2ece92a3132064ade1e0e62c49cdc7e770
jonnicholson94 <jon.nicholson94@googlemail.com>
2023-04-12 13:00:02 +0100

    Now pushing some touch events - so works on mobile (hopefully)

--- elements/DashboardListItem.tsx
+++ elements/DashboardListItem.tsx
@@ -11,7 +11,7 @@ type Props = {
 
 const DashboardListItem = ({ title, employer, start, end, onDragOver, onDragStart, onDrop }: Props) => {
     return (
-        <div className="dashboard-list width-100 flex-center" draggable onDragOver={onDragOver} onDragStart={onDragStart} onDrop={onDrop} id={title}>
+        <div className="dashboard-list width-100 flex-center" draggable onDragOver={onDragOver} onDragStart={onDragStart} onDrop={onDrop} onTouchStart={onDragStart} onTouchEnd={onDrop} onTouchMove={onDragOver} id={title}>
             <p className="dashboard-list-item list-main width-30">{title}</p>
             <p className="dashboard-list-item width-30">{employer}</p>
             <p className="dashboard-list-item width-20">{start}</p>
